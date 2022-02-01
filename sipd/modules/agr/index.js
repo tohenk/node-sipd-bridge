f5c3de26f4f356acf74f4980496fde52538241b1
Toha <tohenk@yahoo.com>
2022-02-01 17:19:55 +0700

    Import AGR class and writer.
    Signed-off-by: Toha <tohenk@yahoo.com>

---
+++ sipd/modules/agr/index.js
@@ -0,0 +1,214 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Toha <tohenk@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const path = require('path');
+const Excel = require('exceljs');
+const Queue = require('@ntlab/ntlib/queue');
+const SipdAgrWriter = require('./writer');
+const SipdUtil = require('../../util');
+
+class SipdAgr {
+    items = []
+
+    import(data) {
+        if (Array.isArray(data)) {
+            data.forEach(row => {
+                let kodeSubKeg = SipdUtil.cleanKode(row.kode_sub_giat);
+                let subkeg = this.getSubKeg(kodeSubKeg);
+                if (!subkeg) {
+                    subkeg = new SipdAgrSubKeg(kodeSubKeg, SipdUtil.cleanText(row.nama_sub_giat));
+                    this.items.push(subkeg);
+                }
+                subkeg.import(row);
+            });
+        }
+    }
+
+    exportXls(outdir) {
+        return new Promise((resolve, reject) => {
+            const q = new Queue(this.items, (subkeg) => {
+                const filename = path.join(outdir, subkeg.kode + '.xlsx');
+                console.log('Writing %s...', filename);
+                const wb = new Excel.Workbook();
+                const sheet = wb.addWorksheet(subkeg.kode);
+                const writer = new SipdAgrWriter(sheet);
+                writer.write(subkeg);
+                wb.xlsx.writeFile(filename);
+                q.next();
+            });
+            q.once('done', () => resolve());
+        });
+    }
+
+    getSubKeg(kode) {
+        let result;
+        this.items.forEach(item => {
+            if (item.kode == kode) {
+                result = item;
+                return true;
+            }
+        });
+        return result;
+    }
+
+    clear() {
+        this.items = [];
+    }
+}
+
+class SipdAgrSubKeg {
+    items = []
+
+    constructor(kode, nama) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+
+    import(data) {
+        this.fromJson(data);
+        this.importPek(data);
+    }
+
+    importPek(data) {
+        let s = SipdUtil.cleanText(data.subs_bl_teks);
+        let pek = this.getPek(s);
+        if (!pek) {
+            pek = new SipdAgrPek(s);
+            this.items.push(pek);
+        }
+        pek.import(data);
+    }
+
+    fromJson(data) {
+        this.kode_skpd = SipdUtil.cleanKode(data.kode_skpd);
+        this.nama_skpd = data.nama_skpd;
+        this.kode_keg = SipdUtil.cleanKode(data.kode_giat);
+        this.nama_keg = data.nama_giat;
+    }
+
+    getPek(pek) {
+        let result;
+        this.items.forEach(item => {
+            if (item.nama == pek) {
+                result = item;
+                return true;
+            }
+        });
+        return result;
+    }
+}
+
+class SipdAgrPek {
+    items = []
+
+    constructor(nama) {
+        this.nama = nama;
+    }
+
+    import(data) {
+        this.fromJson(data);
+        this.importRek(data);
+    }
+
+    importRek(data) {
+        let [krek, nrek] = SipdUtil.splitKode(data.nama_akun);
+        let kodeRek = SipdUtil.cleanKode(krek);
+        let rek = this.getRek(kodeRek);
+        if (!rek) {
+            rek = new SipdAgrRek(kodeRek, nrek);
+            this.items.push(rek);
+        }
+        rek.import(data);
+    }
+
+    fromJson(data) {
+    }
+
+    getRek(rek) {
+        let result;
+        this.items.forEach(item => {
+            if (item.kode == rek) {
+                result = item;
+                return true;
+            }
+        });
+        return result;
+    }
+}
+
+class SipdAgrRek {
+    items = []
+
+    constructor(kode, nama) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+
+    import(data) {
+        this.fromJson(data);
+        this.importRinci(data);
+    }
+
+    importRinci(data) {
+        let rinci = new SipdAgrRinci(data);
+        this.items.push(rinci);
+    }
+
+    fromJson(data) {
+    }
+}
+
+class SipdAgrRinci {
+
+    constructor(data) {
+        this.fromJson(data);
+    }
+
+    fromJson(data) {
+        this.ref = data.id_rinci_sub_bl;
+        this.jenis = data.jenis_bl;
+        this.ssh = data.kode_standar_harga;
+        this.uraian = SipdUtil.cleanText(data.nama_standar_harga.nama_komponen);
+        this.spek = data.nama_standar_harga.spek_komponen;
+        this.satuan = data.satuan;
+        this.harga = SipdUtil.makeFloat(data.harga_satuan);
+        this.volume = SipdUtil.makeFloat(data.volume);
+        this.total = SipdUtil.makeFloat(data.rincian);
+        this.pajak = SipdUtil.makeFloat(data.pajak);
+        if (['hibah', 'bansos'].indexOf(this.jenis) >= 0) {
+            const penerima = SipdUtil.cleanText(data.lokus_akun_teks);
+            let uraian = SipdUtil.cleanText(data.ket_bl_teks);
+            if (uraian) {
+                if (uraian.indexOf(penerima) < 0) {
+                    this.spek = uraian;
+                    uraian = penerima;
+                }
+                this.uraian = uraian;
+            }
+        }
+        if (!this.satuan) this.satuan = data.sat_1;
+    }
+}
+
+module.exports = SipdAgr;
---
+++ sipd/modules/agr/writer.js
@@ -0,0 +1,139 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Toha <tohenk@yahoo.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+class SipdAgrMetaData {
+
+    constructor() {
+        this.column = Object.freeze({
+            REKENING: 'rekening',
+            JENIS: 'jenis',
+            URAIAN: 'uraian',
+            SPEK: 'spek',
+            KODE: 'kode',
+            VOLUME: 'volume',
+            SATUAN: 'satuan',
+            HARGA: 'harga',
+            TOTAL: 'total',
+            PAJAK: 'pajak',
+            REF: 'ref',
+        });
+        this.columns = {
+            [this.column.REKENING]: 'Kode Rekening',
+            [this.column.JENIS]: 'Jenis',
+            [this.column.URAIAN]: 'Uraian',
+            [this.column.SPEK]: 'Spek',
+            [this.column.KODE]: 'Kode',
+            [this.column.VOLUME]: 'Volume',
+            [this.column.SATUAN]: 'Satuan',
+            [this.column.HARGA]: 'Harga Satuan',
+            [this.column.TOTAL]: 'Total',
+            [this.column.PAJAK]: 'Pajak',
+            [this.column.REF]: 'Ref',
+        }
+        this.headers = [
+            {r: 1, key: 'SKPD', var1: 'kode_skpd', var2: 'nama_skpd'},
+            {r: 2, key: 'Kegiatan', var1: 'kode_keg', var2: 'nama_keg'},
+            {r: 3, key: 'Sub Kegiatan', var1: 'kode', var2: 'nama'},
+        ];
+        this.datatype = Object.freeze({SUBKEG: 'SUB', REKENING: 'REK', URAIAN: 'UR'});
+        this.datarow = 6;
+    }
+}
+
+class SipdAgrWriter {
+
+    constructor(sheet) {
+        this.sheet = sheet;
+        this.metadata = new SipdAgrMetaData();
+        this.pos = this.metadata.datarow;
+    }
+
+    nextRow(nextRow = true) {
+        if (nextRow) this.pos++;
+        this.row = this.sheet.getRow(this.pos);
+    }
+
+    rowset(col, value) {
+        if (this.row) {
+            return this.row.getCell(this.columns[col]).value = value;
+        }
+    }
+
+    write(agr) {
+        this.writeHeader(agr);
+        let seq = 0;
+        agr.items.forEach(pek => this.writePek(pek, ++seq));
+    }
+
+    writeHeader(data) {
+        this.columns = {};
+        this.metadata.headers.forEach(info => {
+            const row = this.sheet.getRow(info.r);
+            row.getCell(1).value = info.key;
+            row.getCell(2).value = data[info.var1] ? data[info.var1] : null;
+            row.getCell(3).value = data[info.var2] ? data[info.var2] : null;
+        });
+        this.nextRow(false);
+        let col = 0;
+        Object.keys(this.metadata.columns).forEach(key => {
+            col++;
+            this.row.getCell(col).value = this.metadata.columns[key];
+            this.columns[key] = col;
+        });
+    }
+
+    writePek(agr, seq) {
+        this.nextRow();
+        this.rowset(this.metadata.column.REKENING, seq);
+        this.rowset(this.metadata.column.JENIS, this.metadata.datatype.SUBKEG);
+        this.rowset(this.metadata.column.URAIAN, agr.nama);
+        agr.items.forEach(rek => this.writeRek(rek));
+    }
+
+    writeRek(agr) {
+        this.nextRow();
+        this.rowset(this.metadata.column.REKENING, agr.kode);
+        this.rowset(this.metadata.column.JENIS, this.metadata.datatype.REKENING);
+        this.rowset(this.metadata.column.URAIAN, agr.nama);
+        let seq = 0;
+        agr.items.forEach(rinci => this.writeRinci(rinci, ++seq));
+    }
+
+    writeRinci(agr, seq) {
+        this.nextRow();
+        this.rowset(this.metadata.column.REKENING, seq);
+        this.rowset(this.metadata.column.JENIS, this.metadata.datatype.URAIAN);
+        this.rowset(this.metadata.column.URAIAN, agr.uraian);
+        this.rowset(this.metadata.column.SPEK, agr.spek);
+        this.rowset(this.metadata.column.KODE, agr.ssh);
+        this.rowset(this.metadata.column.SATUAN, agr.satuan);
+        this.rowset(this.metadata.column.VOLUME, agr.volume);
+        this.rowset(this.metadata.column.HARGA, agr.harga);
+        this.rowset(this.metadata.column.TOTAL, agr.total);
+        this.rowset(this.metadata.column.PAJAK, agr.pajak);
+        this.rowset(this.metadata.column.REF, agr.ref);
+    }
+}
+
+module.exports = SipdAgrWriter;
